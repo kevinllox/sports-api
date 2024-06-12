0f8c58bde1e12e3f64b9cd0391d207cb534a9d77
Kevin Grande <kevinbussines2020@gmail.com>
2024-06-12 01:19:13 -0600

    Add migration

---
+++ prisma/migrations/20240612071840_v_1/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "equipos" (
+    "idEquipo" SERIAL NOT NULL,
+    "nombreEquipo" VARCHAR(255) NOT NULL,
+    "institucion" TEXT NOT NULL,
+    "departamento" TEXT NOT NULL,
+    "municipio" TEXT NOT NULL,
+    "direccion" TEXT NOT NULL,
+    "telefono" TEXT NOT NULL,
+
+    CONSTRAINT "equipos_pkey" PRIMARY KEY ("idEquipo")
+);
+
+-- CreateTable
+CREATE TABLE "jugadores" (
+    "idJugador" SERIAL NOT NULL,
+    "nombres" VARCHAR(30) NOT NULL,
+    "apellidos" VARCHAR(30) NOT NULL,
+    "fechaNacimiento" TIMESTAMP(3) NOT NULL,
+    "genero" CHAR(1) NOT NULL,
+    "posicion" VARCHAR(30) NOT NULL,
+    "idEquipo" INTEGER NOT NULL,
+
+    CONSTRAINT "jugadores_pkey" PRIMARY KEY ("idJugador")
+);
+
+-- AddForeignKey
+ALTER TABLE "jugadores" ADD CONSTRAINT "jugadores_idEquipo_fkey" FOREIGN KEY ("idEquipo") REFERENCES "equipos"("idEquipo") ON DELETE RESTRICT ON UPDATE CASCADE;
